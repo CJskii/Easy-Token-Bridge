0fc99837bd36838802da5ed8ba4b76ed28ece7f2
CJskii <czareczarek3@gmail.com>
2023-11-20 21:36:39 +0000

    Add daisyUI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,26 @@
-import Head from "next/head";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center gap-4 p-8">
-        <h1 className="text-3xl text-white">Swing</h1>
-        <div className="flex items-center justify-center gap-2">
-          <Link
-            href="/swing/widget/swap"
-            className="rounded-lg border-2 border-black px-4 py-2 text-white"
-          >
+      <div className="w-100 grid min-h-full grid-flow-col">
+        <div className="flex flex-col items-center justify-center gap-4 p-8">
+          <h1 className="text-3xl text-white">Swing</h1>
+          <div className="flex items-center justify-center gap-2">
+            <Link href="/swing/widget/swap" className="btn">
+              Swap{" "}
+            </Link>
+            <Link href="/swing/widget/gas" className="btn">
+              Gas
+            </Link>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center gap-4 p-8">
+          <h1 className="text-3xl text-white">Squid</h1>
+          <Link href="/squid/widget/swap" className="btn">
             Swap{" "}
           </Link>
-          <Link
-            href="/swing/widget/gas"
-            className="rounded-lg border-2 border-black px-4 py-2 text-white"
-          >
-            Gas
-          </Link>
         </div>
-      </div>
-      <div className="flex flex-col items-center justify-center gap-4 p-8">
-        <h1 className="text-3xl text-white">Squid</h1>
-        <Link
-          href="/squid/widget/swap"
-          className="rounded-lg border-2 border-black px-4 py-2 text-white"
-        >
-          Swap{" "}
-        </Link>
       </div>
     </>
   );

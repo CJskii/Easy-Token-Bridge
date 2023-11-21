776695261981df4f3062808c959cba81eaaf2c59
CJskii <czareczarek3@gmail.com>
2023-11-21 17:42:37 +0000

    revert changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,23 +12,17 @@ const config = {
   ],
   rules: {
     // These opinionated rules are enabled in stylistic-type-checked above.
-    // Feel free to reconfigure them to your own preference.{
-    "@typescript-eslint/no-unsafe-return": "off",
-    "@typescript-eslint/consistent-type-imports": "off",
-    "@typescript-eslint/no-unsafe-assignment": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-floating-promises": "off",
-    "@typescript-eslint/no-unsafe-argument": "off",
-    "react-hooks/exhaustive-deps": "off",
-    "@typescript-eslint/require-await": "off",
-    "@typescript-eslint/no-unsafe-call": "off",
-    "@typescript-eslint/no-unsafe-member-access": "off",
-
+    // Feel free to reconfigure them to your own preference.
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
-    "react/no-unescaped-entities": "off",
-    "@next/next/no-page-custom-font": "off",
 
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/no-misused-promises": [
       2,
